969a872dfdae0ee508c3822ef8fb160f1892f382
Mahmudul Hasan <112176017+mHasan1037@users.noreply.github.com>
2022-08-30 11:02:09 +0600

    Update header.js

--- header.js
+++ header.js
@@ -229,10 +229,11 @@ customElements.define('my-header', MyHeader)
 
 
 
-const activePage = window.location.pathname;
+/*const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('.top-nav a').
 forEach(link => {
     if(link.href.includes(`${activePage}`)){
         link.classList.add('active')
     }
 })
+*/
